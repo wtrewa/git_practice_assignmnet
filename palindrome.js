367c00eb3d6c2a6f6d5e912649b8a3de16e97814
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-12-01 20:30:38 +0530

    fixed prime and added palindrome

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+let str = 'madam';
+let present = false;
+
+for(let i=0;i<str.length;i++) {
+if(str[i] == str[str.length-1-i]) {
+palindrome = true;
+} 
+}
+
+if(palindrome) {
+ console.log(palindrome);
+} else {
+console.log(palindrome);
+}
